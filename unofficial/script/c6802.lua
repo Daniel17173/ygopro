db29adaa797bab502fbc0e68fa1d046578483b9c
daniel17173 <Daniel17173@gmail.com>
2015-10-22 14:25:15 +1100

    ID&pics fix

--- unofficial/script/c6802.lua
+++ unofficial/script/c6802.lua
@@ -1,5 +1,5 @@
 --DD魔導賢者ニュートン
-function c6804.initial_effect(c)
+function c6802.initial_effect(c)
 	--pendulum summon
 	aux.AddPendulumProcedure(c)
 	--Activate
@@ -15,8 +15,8 @@ function c6804.initial_effect(c)
 	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
 	e2:SetTargetRange(1,0)
-	e2:SetCondition(c6804.splimcon)
-	e2:SetTarget(c6804.splimit)
+	e2:SetCondition(c6802.splimcon)
+	e2:SetTarget(c6802.splimit)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_COUNTER)
@@ -25,8 +25,8 @@ function c6804.initial_effect(c)
 	e3:SetCode(EVENT_CHAINING)
 	e3:SetRange(LOCATION_PZONE)
 	e3:SetCountLimit(1)
-	e3:SetCondition(c6804.damcon1)
-	e3:SetOperation(c6804.ctop)
+	e3:SetCondition(c6802.damcon1)
+	e3:SetOperation(c6802.ctop)
 	c:RegisterEffect(e3)
 	--tohand
 	local e4=Effect.CreateEffect(c)
@@ -35,21 +35,21 @@ function c6804.initial_effect(c)
 	e4:SetType(EFFECT_TYPE_IGNITION)
 	e4:SetRange(LOCATION_HAND)
 	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e4:SetCountLimit(1,6804)
-	e4:SetCost(c6804.thcost)
-	e4:SetTarget(c6804.thtg)
-	e4:SetOperation(c6804.thop)
+	e4:SetCountLimit(1,6802)
+	e4:SetCost(c6802.thcost)
+	e4:SetTarget(c6802.thtg)
+	e4:SetOperation(c6802.thop)
 	c:RegisterEffect(e4)
 
 end
-function c6804.splimcon(e)
+function c6802.splimcon(e)
 	return not e:GetHandler():IsForbidden()
 end
-function c6804.splimit(e,c,sump,sumtype,sumpos,targetp)
+function c6802.splimit(e,c,sump,sumtype,sumpos,targetp)
 	return not c:IsSetCard(0xaf) and bit.band(sumtype,SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM
 end
 
-function c6804.damcon1(e,tp,eg,ep,ev,re,r,rp)
+function c6802.damcon1(e,tp,eg,ep,ev,re,r,rp)
 	if not re:GetHandler():IsType(TYPE_TRAP) then return false end
 	local e1=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_DAMAGE)
 	local e2=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_RECOVER)
@@ -62,7 +62,7 @@ function c6804.damcon1(e,tp,eg,ep,ev,re,r,rp)
 	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_RECOVER)
 	return ex and (cp==tp or cp==PLAYER_ALL) and rr and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE)
 end
-function c6804.ctop(e,tp,eg,ep,ev,re,r,rp)
+function c6802.ctop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local c=e:GetHandler()
 		local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
@@ -72,12 +72,12 @@ function c6804.ctop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 		e1:SetTargetRange(1,0)
 		e1:SetLabel(cid)
-		e1:SetValue(c6804.damval)
+		e1:SetValue(c6802.damval)
 		e1:SetReset(RESET_CHAIN)
 		Duel.RegisterEffect(e1,tp)
 	Duel.Destroy(c,REASON_EFFECT)
 end
-function c6804.damval(e,re,val,r,rp,rc)
+function c6802.damval(e,re,val,r,rp,rc)
 	local cc=Duel.GetCurrentChain()
 	if cc==0 or bit.band(r,REASON_EFFECT)==0 then return val end
 	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
@@ -85,21 +85,21 @@ function c6804.damval(e,re,val,r,rp,rc)
 	return 0
 end
 
-function c6804.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c6802.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsDiscardable() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
 end
-function c6804.thfilter(c)
+function c6802.thfilter(c)
 	return c:IsFaceup() and (c:IsSetCard(0xae) or c:IsSetCard(0xaf)) and c:IsAbleToHand()
 end
-function c6804.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and c6804.thfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c6804.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+function c6802.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and c6802.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c6802.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-	local g=Duel.SelectTarget(tp,c6804.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,c6802.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
-function c6804.thop(e,tp,eg,ep,ev,re,r,rp)
+function c6802.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
