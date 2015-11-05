1a9feac6917ca98c8a2d74b59853d2238be3ade5
mercury233 <me@mercury233.me>
2015-11-05 18:32:39 +0800

    fix Kozmoll Wickedwitch

--- unofficial/script/c93302695.lua
+++ unofficial/script/c93302695.lua
@@ -20,6 +20,7 @@ function c93302695.initial_effect(c)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
 	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCost(c93302695.indcost)
 	e2:SetOperation(c93302695.operation)
 	c:RegisterEffect(e2)
 end
@@ -42,6 +43,10 @@ function c93302695.spop(e,tp,eg,ep,ev,re,r,rp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
+end
+function c93302695.indcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
 end
 function c93302695.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
