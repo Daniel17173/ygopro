ac316b0ab7fdbca000725d3daa7c7f86ade64041
mercury233 <me@mercury233.me>
2015-11-11 22:49:06 +0800

    replace 906 TCG&KCG with offical code

--- unofficial/script/c93124273.lua
+++ unofficial/script/c93124273.lua
@@ -1,6 +1,6 @@
---Samurai Cavalry of Reptier
+--Samurai Calvary of Reptier
 function c93124273.initial_effect(c)
-	--Pendulum Summon
+	--pendulum summon
 	aux.AddPendulumProcedure(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -8,24 +8,24 @@ function c93124273.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
 	--destroy
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(61132951,0))
-	e1:SetCategory(CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_BATTLE_START)
-	e1:SetTarget(c93124273.destg)
-	e1:SetOperation(c93124273.desop)
-	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(93124273,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_START)
+	e2:SetTarget(c93124273.target)
+	e2:SetOperation(c93124273.operation)
+	c:RegisterEffect(e2)
 end
-
-function c93124273.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local d=e:GetHandler():GetBattleTarget()
-	if chk==0 then return Duel.GetAttacker()==e:GetHandler() and d and d:IsFaceup() and not d:IsType(TYPE_PENDULUM) and d:IsControler(1-tp) and d:IsDestructable() end
+function c93124273.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local d=Duel.GetAttackTarget()
+	if chk ==0 then	return Duel.GetAttacker()==e:GetHandler()
+		and d and d:IsFaceup() and not d:IsType(TYPE_PENDULUM) and d:IsDestructable() end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,d,1,0,0)
 end
-function c93124273.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetHandler():GetBattleTarget()
-	if tc:IsRelateToBattle() then
-		Duel.Destroy(tc,REASON_EFFECT)
+function c93124273.operation(e,tp,eg,ep,ev,re,r,rp)
+	local d=Duel.GetAttackTarget()
+	if d:IsRelateToBattle() then
+		Duel.Destroy(d,REASON_EFFECT)
 	end
 end
