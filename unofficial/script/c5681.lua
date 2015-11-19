4843d8119bd79e36a18127b01885a99969571a04
daniel17173 <Daniel17173@gmail.com>
2015-11-19 15:24:12 +1100

    908 MP01 vjmp new cards update

--- unofficial/script/c5681.lua
+++ unofficial/script/c5681.lua
@@ -1,85 +1,66 @@
---青き眼の巫女
+--青眼の双爆裂龍
 function c5681.initial_effect(c)
-	--spsummon
+	--fusion material
+	c:EnableReviveLimit()
+	aux.AddFusionProcCodeRep(c,89631139,2,true,true)
+	--special summon rule
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(5681,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EVENT_BECOME_TARGET)
-	e1:SetCountLimit(1,5681)
-	e1:SetCost(c5681.cost)
-	e1:SetCondition(c5681.tgcon)
-	e1:SetTarget(c5681.target)
-	e1:SetOperation(c5681.activate)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_EXTRA)
+	e1:SetCondition(c5681.spcon)
+	e1:SetOperation(c5681.spop)
 	c:RegisterEffect(e1)
-	--special summon
+	--battle indestructable
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(5681,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,5681)
-	e2:SetTarget(c5681.sptg2)
-	e2:SetOperation(c5681.spop2)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetValue(1)
 	c:RegisterEffect(e2)
+	--Extra attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_EXTRA_ATTACK)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	--Banish
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_REMOVE)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_DAMAGE_STEP_END)
+	e4:SetCondition(c5681.rmcon)
+	e4:SetOperation(c5681.rmop)
+	c:RegisterEffect(e4)
 end
-function c5681.costfilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and c:IsAbleToGraveAsCost()
+function c5681.spfilter(c)
+	return c:IsFaceup() and c:IsCode(89631139) and c:IsCanBeFusionMaterial() and c:IsAbleToGrave()
 end
-function c5681.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c5681.costfilter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c5681.costfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SendtoGrave(g,REASON_COST)
+function c5681.spcon(e,c)
+	if c==nil then return true end
+	return Duel.IsExistingMatchingCard(c5681.spfilter,e:GetHandler():GetControler(),LOCATION_MZONE,0,2,nil)
 end
-function c5681.tgcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsContains(e:GetHandler())
+function c5681.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectMatchingCard(tp,c5681.spfilter,tp,LOCATION_MZONE,0,2,2,nil)
+	Duel.SendtoGrave(g,REASON_COST+REASON_FUSION+REASON_MATERIAL)
 end
-function c5681.filter(c)
-	return (c:IsCode(89631139) or c:IsCode(53347303) or c:IsCode(53183600) or c:IsCode(9433350)) and c:IsAbleToDeck()
-end
-function c5681.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c5681.filter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-end
-function c5681.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c5681.filter,tp,LOCATION_DECK,0,nil)
-	if g:GetCount()==0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g1=g:Select(tp,1,1,nil)
-	local t={89631139,53347303,53183600,9433350}
-	for i=1,4 do
-		if g1:GetFirst():IsCode(t[i]) then g:Remove(Card.IsCode,nil,t[i]) end
-	end
-	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(6595475,0)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g2=g:Select(tp,1,1,nil)
-		g1:Merge(g2)
-	end
-	Duel.SendtoHand(g1,nil,REASON_EFFECT)
-	Duel.ConfirmCards(1-tp,g1)
-end
-function c5681.bewdfilter(c)
-	return (c:IsCode(89631139) or c:IsCode(53347303) or c:IsCode(53183600) or c:IsCode(9433350) or c:IsCode(23995346)) and c:IsAbleToDeck()
-end
-function c5681.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and Duel.IsExistingTarget(c5681.bewdfilter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,c5681.bewdfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
-end
-function c5681.spop2(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
+function c5681.rmcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)~=0
-		and tc:IsLocation(LOCATION_DECK+LOCATION_EXTRA) then
-		Duel.ShuffleDeck(tc:GetControler())
-		Duel.BreakEffect()
-		if c:IsRelateToEffect(e) then
-			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-		end
+	local tc
+	if not c:IsRelateToBattle() then return false end
+	if c==Duel.GetAttacker() then
+		tc=Duel.GetAttackTarget()
+	elseif c==Duel.GetAttackTarget() then
+		tc=Duel.GetAttacker()
 	end
+	if tc and tc:IsLocation(LOCATION_MZONE) then
+		e:SetLabelObject(tc)
+		return true
+	else
+		return false
+	end
+end
+function c5681.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
 end
