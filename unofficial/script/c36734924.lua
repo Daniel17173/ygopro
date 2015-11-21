b816cdd8ec109f544f982754de16ff96305e003f
daniel17173 <Daniel17173@gmail.com>
2015-11-21 16:47:47 +1100

    fix

--- unofficial/script/c36734924.lua
+++ unofficial/script/c36734924.lua
@@ -1,85 +1,78 @@
 --青き眼の巫女
 function c36734924.initial_effect(c)
-	--spsummon
+	--search
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(36734924,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SEARCH+CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EVENT_BECOME_TARGET)
 	e1:SetCountLimit(1,36734924)
-	e1:SetCost(c36734924.cost)
-	e1:SetCondition(c36734924.tgcon)
-	e1:SetTarget(c36734924.target)
-	e1:SetOperation(c36734924.activate)
+	e1:SetCondition(c36734924.thcon)
+	e1:SetTarget(c36734924.thtg)
+	e1:SetOperation(c36734924.thop)
 	c:RegisterEffect(e1)
-	--special summon
+	--spsummon
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(36734924,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,36734924)
-	e2:SetTarget(c36734924.sptg2)
-	e2:SetOperation(c36734924.spop2)
+	e2:SetTarget(c36734924.sptg)
+	e2:SetOperation(c36734924.spop)
 	c:RegisterEffect(e2)
 end
-function c36734924.costfilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and c:IsAbleToGraveAsCost()
-end
-function c36734924.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c36734924.costfilter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c36734924.costfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SendtoGrave(g,REASON_COST)
-end
-function c36734924.tgcon(e,tp,eg,ep,ev,re,r,rp)
+function c36734924.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsContains(e:GetHandler())
 end
-function c36734924.filter(c)
-	return (c:IsSetCard(0xe0) or c:IsCode(89631139) or c:IsCode(9433350) or c:IsCode(23995346) or c:IsCode(53183600) or c:IsCode(53347303)) and c:IsAbleToDeck()
+function c36734924.tgfilter(c)
+	return c:IsType(TYPE_EFFECT) and c:IsAbleToGrave()
 end
-function c36734924.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c36734924.filter,tp,LOCATION_DECK,0,1,nil) end
+function c36734924.thfilter(c)
+	return c:IsSetCard(0xdd) and c:IsAbleToHand()
+end
+function c36734924.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c36734924.tgfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(c36734924.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_MZONE)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c36734924.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c36734924.filter,tp,LOCATION_DECK,0,nil)
-	if g:GetCount()==0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g1=g:Select(tp,1,1,nil)
-	local t={89631139,53347303,53183600,9433350}
-	for i=1,4 do
-		if g1:GetFirst():IsCode(t[i]) then g:Remove(Card.IsCode,nil,t[i]) end
-	end
-	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(6595475,0)) then
+function c36734924.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local tg=Duel.SelectMatchingCard(tp,c36734924.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	if tg:GetCount()>0 and Duel.SendtoGrave(tg,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_GRAVE) then
+		local g=Duel.GetMatchingGroup(c36734924.thfilter,tp,LOCATION_DECK,0,nil)
+		if g:GetCount()<=0 then return end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g2=g:Select(tp,1,1,nil)
-		g1:Merge(g2)
+		local sg1=g:Select(tp,1,1,nil)
+		g:Remove(Card.IsCode,nil,sg1:GetFirst():GetCode())
+		if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(36734924,2)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local sg2=g:Select(tp,1,1,nil)
+			sg1:Merge(sg2)
+		end
+		Duel.SendtoHand(sg1,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg1)
 	end
-	Duel.SendtoHand(g1,nil,REASON_EFFECT)
-	Duel.ConfirmCards(1-tp,g1)
 end
-function c36734924.bewdfilter(c)
-	return (c:IsCode(89631139) or c:IsCode(53347303) or c:IsCode(53183600) or c:IsCode(9433350) or c:IsCode(23995346)) and c:IsAbleToDeck()
+function c36734924.spfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xdd) and c:IsAbleToDeck()
 end
-function c36734924.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and Duel.IsExistingTarget(c36734924.bewdfilter,tp,LOCATION_MZONE,0,1,nil) end
+function c36734924.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c36734924.spfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c36734924.spfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>-1 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,c36734924.bewdfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,c36734924.spfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c36734924.spop2(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
+function c36734924.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)~=0
-		and tc:IsLocation(LOCATION_DECK+LOCATION_EXTRA) then
-		Duel.ShuffleDeck(tc:GetControler())
-		Duel.BreakEffect()
-		if c:IsRelateToEffect(e) then
-			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-		end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)~=0 and c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
