0c887602ce531aca78a78611893f1167051ff25f
mercury233 <me@mercury233.me>
2015-11-26 19:00:31 +0800

    fix 裏風の精霊

--- unofficial/script/c5839.lua
+++ unofficial/script/c5839.lua
@@ -2,7 +2,7 @@
 function c5839.initial_effect(c)
 	--summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
