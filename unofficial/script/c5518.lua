b63dc59f3dffedf93c267e303d46ac6127a9b211
mercury233 <me@mercury233.me>
2015-12-05 21:17:57 +0800

    fix 左腕の代償

--- unofficial/script/c5518.lua
+++ unofficial/script/c5518.lua
@@ -5,17 +5,32 @@ function c5518.initial_effect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(c5518.condition)
 	e1:SetCost(c5518.cost)
 	e1:SetTarget(c5518.target)
 	e1:SetOperation(c5518.activate)
 	c:RegisterEffect(e1)
+	if not c5518.global_check then
+		c5518.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_SSET)
+		ge1:SetOperation(c5518.setcheck)
+		Duel.RegisterEffect(ge1,0)
+	end
+end
+function c5518.setcheck(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(rp,5518,RESET_PHASE+PHASE_END,0,1)
 end
 function c5518.cfilter(c)
-	return not c:IsAbleToRemoveAsCost()
+	return c:IsAbleToRemoveAsCost()
+end
+function c5518.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFlagEffect(tp,5518)==0
 end
 function c5518.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
-	if chk==0 then return g:GetCount()>=2 and not g:IsExists(c5518.cfilter,1,nil) end
+	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0):Filter(c5518.cfilter,e:GetHandler())
+	if chk==0 then return g:GetCount()>=2 end
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 	--cannot set
 	local e1=Effect.CreateEffect(e:GetHandler())
