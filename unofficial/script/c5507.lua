3e5bf459ff528d04af13ae08f23317f2364ab977
mercury233 <me@mercury233.me>
2015-12-05 17:34:21 +0800

    fix 獄炎のカース・オブ・ドラゴン

--- unofficial/script/c5507.lua
+++ unofficial/script/c5507.lua
@@ -2,7 +2,7 @@
 function c5507.initial_effect(c)
 	--destroy
 	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
@@ -10,7 +10,7 @@ function c5507.initial_effect(c)
 	e1:SetOperation(c5507.desop)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e2:SetCategory(CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -30,10 +30,10 @@ function c5507.filter(c)
 	return c:IsFaceup() and c:IsType(TYPE_FIELD) and c:IsDestructable()
 end
 function c5507.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and c5507.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c5507.filter,tp,0,LOCATION_ONFIELD,1,nil) end
+	if chkc then return chkc:IsOnField() and c5507.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c5507.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,c5507.filter,tp,0,LOCATION_ONFIELD,1,1,nil)
+	local g=Duel.SelectTarget(tp,c5507.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function c5507.desop(e,tp,eg,ep,ev,re,r,rp)
