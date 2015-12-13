0de468c8d9519073f4a6e671c9314992266499c8
mercury233 <me@mercury233.me>
2015-12-13 22:11:31 +0800

    add 造反劇 script

---
+++ unofficial/script/c5516.lua
@@ -0,0 +1,49 @@
+--造反劇
+--Script by mercury233
+function c5516.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_CONTROL)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCountLimit(1,5516)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_BATTLE_START,TIMING_BATTLE_START)
+	e1:SetCondition(c5516.condition)
+	e1:SetTarget(c5516.target)
+	e1:SetOperation(c5516.activate)
+	c:RegisterEffect(e1)
+end
+function c5516.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()==PHASE_BATTLE and Duel.GetActivityCount(tp,ACTIVITY_ATTACK)==0
+end
+function c5516.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:GetLocation()==LOCATION_MZONE and chkc:GetControler()~=tp and chkc:IsControlerCanBeChanged() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,nil)  end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	local g=Duel.SelectTarget(tp,Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
+end
+function c5516.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		if not Duel.GetControl(tc,tp,PHASE_BATTLE,1) then
+			if not tc:IsImmuneToEffect(e) and tc:IsAbleToChangeControler() then
+				Duel.Destroy(tc,REASON_EFFECT)
+			end
+			return
+		end
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetTargetRange(LOCATION_MZONE,0)
+		e1:SetTarget(c5516.ftarget)
+		e1:SetLabel(tc:GetFieldID())
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function c5516.ftarget(e,c)
+	return e:GetLabel()~=c:GetFieldID()
+end
