7f0c8e0d54ec966a0ecf189879430207b5aebe5a
mercury233 <me@mercury233.me>
2015-12-17 19:10:47 +0800

    fix Moon-Light Blue Cat

--- unofficial/script/c5808.lua
+++ unofficial/script/c5808.lua
@@ -21,7 +21,7 @@ function c5808.initial_effect(c)
 	c:RegisterEffect(e4)
 end
 function c5808.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0x209)
+	return c:IsFaceup() and c:IsSetCard(0x209) and not c:IsCode(5808)
 end
 function c5808.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c5808.filter(chkc) end
@@ -43,18 +43,18 @@ end
 function c5808.regcon(e,tp,eg,ep,ev,re,r,rp)
 	return bit.band(r,REASON_EFFECT+REASON_BATTLE)
 end
-function c5808.filter(c,e,tp)
+function c5808.filter2(c,e,tp)
 	return c:IsSetCard(0x209) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c5808.regtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c5808.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c5808.filter2,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function c5808.regop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c5808.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c5808.filter2,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
