7ab776f644e81776f716bacf5604d2fbed0bb771
mercury233 <me@mercury233.me>
2016-01-21 13:26:52 +0800

    Revert "月光紅狐 id update"
    This reverts commit bf21089c45786e44c26469debdec6ab8946d8587.

--- unofficial/script/c5681.lua
+++ unofficial/script/c5681.lua
@@ -1,41 +1,41 @@
 --月光紅狐
 --Lunalight Crimson Fox
 --Scripted by Eerie Code-6118
-function c94919024.initial_effect(c)
+function c5681.initial_effect(c)
 	--ATK to 0
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(94919024,0))
+	e1:SetDescription(aux.Stringid(5681,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_TO_GRAVE)
-	e1:SetCondition(c94919024.atkcon)
-	e1:SetTarget(c94919024.atktg)
-	e1:SetOperation(c94919024.atkop)
+	e1:SetCondition(c5681.atkcon)
+	e1:SetTarget(c5681.atktg)
+	e1:SetOperation(c5681.atkop)
 	c:RegisterEffect(e1)
 	--Negate
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(94919024,1))
+	e2:SetDescription(aux.Stringid(5681,1))
 	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_RECOVER)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCondition(c94919024.condition)
-	e2:SetCost(c94919024.cost)
-	e2:SetTarget(c94919024.target)
-	e2:SetOperation(c94919024.operation)
+	e2:SetCondition(c5681.condition)
+	e2:SetCost(c5681.cost)
+	e2:SetTarget(c5681.target)
+	e2:SetOperation(c5681.operation)
 	c:RegisterEffect(e2)
 end
-function c94919024.atkcon(e,tp,eg,ep,ev,re,r,rp)
+function c5681.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsReason(REASON_EFFECT)
 end
-function c94919024.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c5681.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and aux.nzatk(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(aux.nzatk,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,aux.nzatk,tp,0,LOCATION_MZONE,1,1,nil)
 end
-function c94919024.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c5681.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(e:GetHandler())
@@ -46,24 +46,24 @@ function c94919024.atkop(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e1)
 	end
 end
-function c94919024.filter(c,tp)
+function c5681.filter(c,tp)
 	return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsSetCard(0xdf)
 end
-function c94919024.condition(e,tp,eg,ep,ev,re,r,rp)
+function c5681.condition(e,tp,eg,ep,ev,re,r,rp)
 	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
 	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-	return g and g:IsExists(c94919024.filter,1,nil,tp)
+	return g and g:IsExists(c5681.filter,1,nil,tp)
 		and Duel.IsChainNegatable(ev)
 end
-function c94919024.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c5681.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
-function c94919024.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c5681.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 end
-function c94919024.operation(e,tp,eg,ep,ev,re,r,rp)
+function c5681.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) then
 		Duel.Recover(tp,1000,REASON_EFFECT)
 		Duel.Recover(1-tp,1000,REASON_EFFECT)
