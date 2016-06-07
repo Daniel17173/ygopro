0676fb4751b14bb37e9e795a04af95bf4daba4d0
mercury233 <me@mercury233.me>
2016-06-07 20:50:29 +0800

    update scripts

--- unofficial/script/c100301005.lua
+++ unofficial/script/c100301005.lua
@@ -58,12 +58,12 @@ function c100301005.spcost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
 function c100301005.spfilter2(c,e,tp)
-	return c:IsSetCard(0xa4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0xa4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsType(TYPE_MONSTER)
 end
 function c100301005.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100301005.spfilter2(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		Duel.IsExistingTarget(c100301005.spfilter2,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+		and Duel.IsExistingTarget(c100301005.spfilter2,tp,LOCATION_GRAVE,0,1,e:GetHandler(),e,tp) end
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
