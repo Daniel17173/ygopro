78bc6afad27ee519b0cd85d17ff03763d97051cd
mercury233 <me@mercury233.me>
2016-07-18 01:55:54 +0800

    update and fix

--- unofficial/script/c100405017.lua
+++ unofficial/script/c100405017.lua
@@ -20,6 +20,7 @@ function c100405017.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_ATKCHANGE)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
 	e2:SetCondition(c100405017.atkcon2)
 	e2:SetOperation(c100405017.atkop2)
@@ -75,7 +76,7 @@ end
 function c100405017.atkop2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsFaceup() and c:IsRelateToEffect(e) then
-		local atkval=Duel.GetMatchingGroupCount(c100405017.atkfilter,tp,LOCATION_MZONE,0,nil)*300
+		local atkval=Duel.GetMatchingGroupCount(c100405017.atkfilter2,tp,LOCATION_MZONE,0,nil)*300
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
