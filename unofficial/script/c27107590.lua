16d5c4346dbbab5e7ccdf4ad7d7a9a1e703110be
mercury233 <me@mercury233.me>
2017-05-23 01:26:30 +0800

    Revert "temp fix"
    This reverts commit 7d5122bd12ade7f03c7f86f999b3d6e2187e5f8e.

--- unofficial/script/c27107590.lua
+++ unofficial/script/c27107590.lua
@@ -44,14 +44,14 @@ function c27107590.sprcon(e,c)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end
 function c27107590.dtcon(e,c)
-	return (c:IsSetCard(0x4a) or c:IsCode(74530899,8967776,28929131,65314286,91712985,92435533))
+	return (c:IsSetCard(0x4a) or c:IsCode(74530899,8967776))
 end
 function c27107590.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsReleasable() end
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
 function c27107590.thfilter(c)
-	return (c:IsSetCard(0x4a) or c:IsCode(74530899,8967776,28929131,65314286,91712985,92435533)) and c:IsType(TYPE_MONSTER) and c:GetAttack()==0 and c:IsAbleToHand()
+	return (c:IsSetCard(0x4a) or c:IsCode(74530899,8967776)) and c:IsType(TYPE_MONSTER) and c:GetAttack()==0 and c:IsAbleToHand()
 end
 function c27107590.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c27107590.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -83,7 +83,7 @@ function c27107590.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return se~=e:GetLabelObject()
 end
 function c27107590.spfilter(c,e,tp)
-	return (c:IsSetCard(0x4a) or c:IsCode(74530899,8967776,28929131,65314286,91712985,92435533)) and c:IsType(TYPE_MONSTER) and c:GetAttack()==0 and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+	return (c:IsSetCard(0x4a) or c:IsCode(74530899,8967776)) and c:IsType(TYPE_MONSTER) and c:GetAttack()==0 and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 function c27107590.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
