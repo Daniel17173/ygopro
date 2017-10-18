32aded5d93f88bcbe51cc3c057b3f375977f5cb1
mercury233 <me@mercury233.me>
2017-10-18 20:40:53 +0800

    update scripts

---
+++ unofficial/script/c50696588.lua
@@ -0,0 +1,4 @@
+--粗人舞导
+function c50696588.initial_effect(c)
+	Debug.Message("「粗人舞导」暂时无法使用！")
+end
---
+++ unofficial/script/c87170768.lua
@@ -0,0 +1,4 @@
+--接触的G
+function c87170768.initial_effect(c)
+	Debug.Message("「接触的G」暂时无法使用！")
+end
